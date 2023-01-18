fe8695b32612c99e6b9d5f2051aeea8ed5e7d4ac
manoshuk100 <95406547+manoshuk100@users.noreply.github.com>
2023-01-18 16:07:20 +0530

    Create Anomaly_Detection.py

---
+++ Anomaly_Detection.py
@@ -0,0 +1,133 @@
+
+"""This script assumes that there is a separate program that checks for each anomaly based on specific threshold limits and returns the results as a dictionary. It also assumes that the anomaly results are binary (i.e., occurred or not occurred).The script uses the anomaly results and the predefined anomalies and clusters to 
+calculate the percentage of occurrences for each cluster, and then uses the cluster percentages and weightings to calculate the overall risk level.The script then uses 
+a while loop to perform the check every 24 hours. You will have to add more details like logging mechanism, threshold limit for each anomaly, how to get the results from separate program and how to trigger alerts based on the overall risk level, and also exception handling. You will also have to make sure that the separate program 
+that you are using is running properly and providing accurate results, as the accuracy of this script depends on the accuracy of the results provided by the separate program."""
+"""
+
+# Script assumes that you are writing the results from a router in a CSV file
+
+import csv
+
+# Define the clusters and weightings
+clusters = {
+    "Highly critical": 0.7,
+    "Moderately critical": 0.2,
+    "Low critical": 0.1
+}
+
+# Define the anomalies and their corresponding clusters
+anomalies = {
+    "CPU more than 70%": "Highly critical",
+    "MTS stuck": "Highly critical",
+    "Power supply failure": "Highly critical",
+    "Consistency check failed": "Highly critical",
+    "Input discards on interface": "Moderately critical",
+    "ARP completion ratio check": "Moderately critical",
+    "ARP counter error check": "Moderately critical",
+    "IPv6 ND completion ratio check": "Moderately critical",
+    "IPv6 ND counter error check": "Low critical",
+    "Ethernet interface error disabled check": "Low critical",
+    "H/w statistics errors": "Low critical",
+    "Configuration saved": "Low critical"
+}
+
+# Perform a periodic check every 24 hours
+def overall risk():
+    while True:
+    # Assume a separate program is checking for each anomaly and returning the result as a dictionary
+    anomaly_results = separate_program()
+    
+    # Initialize counters for each cluster
+    cluster_counters = {cluster: 0 for cluster in clusters.keys()}
+    
+    # Count the number of occurrences for each anomaly
+    for anomaly, result in anomaly_results.items():
+        if result:
+            cluster_counters[anomalies[anomaly]] += 1
+    
+    # Calculate the percentage of occurrences for each cluster
+    cluster_percentages = {cluster: count / len(anomaly_results) for cluster, count in cluster_counters.items()}
+    
+    # Calculate the overall risk level
+    overall_risk = sum([percentage * weight for cluster, (percentage, weight) in zip(cluster_percentages.items(), clusters.items())])
+    
+    # Alert the user based on the overall risk level
+    if overall_risk >= 0.6:
+        print("High risk: Overall risk level is {}%".format(overall_risk * 100))
+    elif overall_risk >= 0.3:
+        print("Medium risk: Overall risk level is {}%".format(overall_risk * 100))
+    else:
+        print("Low risk: Overall risk level is {}%".format(overall_risk * 100))
+    anomaly_results = result()
+    print("Anomaly Results: ", anomaly_results)
+    mitigate_risk(anomaly_results, mitigation_steps)    
+    time.sleep(24 * 60 * 60) # sleep for 24 hours before checking again
+    
+def separate_program():
+    # code to check for CPU usage anomaly
+    cpu_usage = check_cpu_usage()
+    if cpu_usage > 75:
+        cpu_anomaly = True
+    else:
+        cpu_anomaly = False
+    
+    # code to check for input discards on interface anomaly
+    input_discards = check_input_discards()
+    if input_discards > 100:
+        input_discards_anomaly = True
+    else:
+        input_discards_anomaly = False
+        
+    # code to check for other anomalies
+    ...
+    
+    return {
+        "CPU more than 70%": cpu_anomaly,
+        "Input discards on interface": input_discards_anomaly,
+        ...
+    }
+
+def result():
+    anomaly_results = {}
+    with open('anomalies.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            anomaly = row['Anomaly']
+            threshold = row['Threshold']
+            if threshold != "N/A":
+                result = check_anomaly(anomaly, threshold)
+                anomaly_results[anomaly] = result
+            else:
+                anomaly_results[anomaly] = None
+    return anomaly_results
+
+mitigation_steps = {
+    "CPU more than 70%": ["Check for any processes that are using high CPU", "Restart the process or service that is causing high CPU usage", "Add more CPU resources if necessary"],
+    "MTS stuck": ["Check the MTS logs for errors", "Restart the MTS process", "Contact vendor for support if necessary"],
+    "Power supply failure": ["Check the power supply status and replace if necessary", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "Consistency check failed": ["Run consistency check again", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "Input discards on interface": ["Check the interface configuration", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "ARP completion ratio check": ["Check ARP table for any errors", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "ARP counter error check": ["Check ARP table for any errors", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "IPv6 ND completion ratio check": ["Check ND table for any errors", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "IPv6 ND counter error check": ["Check ND table for any errors", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "Ethernet interface error disabled check": ["Check the interface configuration", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "H/w statistics errors": ["Check the hardware for any issues", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+    "Configuration saved": ["Verify the configuration", "Check for any errors in the system logs", "Contact vendor for support if necessary"],
+}
+
+def mitigate_risk(anomaly_results, mitigation_steps):
+    for anomaly, result in anomaly_results.items():
+        if result and anomaly in mitigation_steps:
+            print(f'Mitigating risk for {anomaly}')
+            for step in mitigation_steps[anomaly]:
+                print(f'- {step}')
+                # Code to perform the step
+    
+# main function
+if __name__ == "__main__":
+    overall_risk()
+  
+
+  
